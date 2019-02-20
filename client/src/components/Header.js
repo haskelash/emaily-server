eafeadf1a30137a29e54df539a96fd7addd3b6d0
Haskel Ash <haskelash@gmail.com>
2019-02-20 13:08:10 +0200

    refactor auth action to use async/await, have auth state default to null, be false for logged out, or on object if logged in, unwrap the state in the Header component in mapStateToProps and switch on it to figure out what to show in the header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li><a href="/auth/google">Login with Google</a></li>
+        )
+      default:
+        return (
+          <li><a href="/">Logout</a></li>
+        )
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo" href="/">Emaily</a>
           <ul className="right">
-            <li><a href="/">Login with Google</a></li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
@@ -15,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function (state = {}, action) {
-  console.log(action)
+import { FETCH_USER} from '../actions/types'
+
+export default function (state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      //payload will be empty string if logged out, in that case return false
+      return action.payload || false
     default:
       return state
   }
