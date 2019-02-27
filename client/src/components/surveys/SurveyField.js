b4f8c19465f67e83c091847cd27658cca36f4090
Haskel Ash <haskelash@gmail.com>
2019-02-27 15:01:51 +0200

    extract meta from react-form props, use it to decide whether to show error, add validation func to reduxForm.

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,14 @@
 //SurveyField contains logic to render a single label and text input
 import React from 'react'
 
-export default ({ input, label }) => {
+export default ({ input, meta: { error, touched }, label }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: '5px' }}/>
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+        </div>
     </div>
   )
 }
