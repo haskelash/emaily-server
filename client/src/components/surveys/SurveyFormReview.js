521bca7a0ab04b8a542b04bcdcf380a80fc7fba9
Haskel Ash <haskelash@gmail.com>
2019-03-01 14:27:49 +0200

    pass function to SurveyFormReview to call on back button to go back to form

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,16 @@
+//SurveyFormReview show users their form inputs for review
 import React from 'react'
 
-export default () => {
+export default ({ onCancel }) => {
   return (
     <div>
       <h5>Review</h5>
+      <button
+        className="yellow darken-3 btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
     </div>
   )
 }
