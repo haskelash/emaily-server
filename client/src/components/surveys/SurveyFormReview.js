6499174f21332af35117f5f4630d90650b3dca55
Haskel Ash <haskelash@gmail.com>
2019-03-03 08:16:28 +0200

    add submit button

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -19,10 +19,14 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       <h5>Please confirm your entries</h5>
       {reviewFields}
       <button
-        className="yellow darken-3 btn-flat"
+        className="yellow white-text darken-3 btn-flat"
         onClick={onCancel}
       >
         Back
+      </button>
+      <button className="green white-text btn-flat right">
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   )
