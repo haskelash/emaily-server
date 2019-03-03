bebc6f9169eab8d37c58a1b55bcd86a90e004aad
Haskel Ash <haskelash@gmail.com>
2019-03-03 14:17:43 +0200

    use react-router-dom history helper to tell submit action to push to dashboard on submit

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
+import { withRouter } from 'react-router-dom'
 import formFields from './formFields'
 import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ label, name }) => {
     return (
       <div key={name}>
@@ -26,7 +27,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
         Back
       </button>
       <button
-        onClick={() => submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
         className="green white-text btn-flat right"
       >
         Send Survey
@@ -40,4 +41,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values }
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview)
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview))
