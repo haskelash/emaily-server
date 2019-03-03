f651d94f762ae111572b9d695bbf6d5b75cc786c
Haskel Ash <haskelash@gmail.com>
2019-03-03 08:12:57 +0200

    use externalized formFields in SurveyFormReview to display input for review.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,23 @@
 //SurveyFormReview show users their form inputs for review
 import React from 'react'
 import { connect } from 'react-redux'
+import _ from 'lodash'
+import formFields from './formFields'
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ label, name }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    )
+  })
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      {reviewFields}
       <button
         className="yellow darken-3 btn-flat"
         onClick={onCancel}
